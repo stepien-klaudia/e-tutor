f2684cd6b3d53dab25858c8b17c41659cdf3642f
Klaudia <stepien.klaudia.agata@gmail.com>
2022-12-18 14:45:04 +0100

    paginacja listy uzytkownikow

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
     {
         return view('users.index',
         [
-            'users' => User::all()
+            'users' => User::paginate(3)
         ]
     );
     }
