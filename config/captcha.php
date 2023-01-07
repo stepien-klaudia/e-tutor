b2b51712a3ccc1655417c337f525e8d71ba79511
Klaudia <stepien.klaudia.agata@gmail.com>
2023-01-07 17:59:16 +0100

    modyfikacja konfiguracji captcha.php

--- config/captcha.php
+++ config/captcha.php
@@ -8,7 +8,7 @@ return [
         'width' => 120,
         'height' => 36,
         'quality' => 90,
-        'math' => false,
+        'math' => true,
         'expire' => 60,
         'encrypt' => false,
     ],
